fa74a9558a814aabc1adde4a45c7d40e34294c2d
modesty0310 <rlatprua4@gmail.com>
2024-10-02 14:36:58 +0900

    first commit

---
+++ src/test/java/com/modesty0310/matzip/MatzipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.modesty0310.matzip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatzipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
